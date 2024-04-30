f12d348787902cdc8202832a5a78ec7d519fd3bb
Dagaet <alojera15@gmail.com>
2024-04-30 08:59:42 +0100

    Test(Green): StringCalculatorShould return the same number when given

--- src/main/kotlin/dev/kata/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/dev/kata/stringcalculator/StringCalculator.kt
@@ -3,6 +3,9 @@ package dev.kata.stringcalculator
 class StringCalculator {
     fun add(number: String): Int
     {
+        if (number.isNotEmpty()) {
+            return number.toInt()
+        }
         return 0
     }
 }
